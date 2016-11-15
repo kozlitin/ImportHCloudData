369207966c5d8c257cfe8d6316046c4371925edb
Igor Kozlitin <igor_kozlitin@mail.ru>
2016-11-15 12:15:35 +0200

    simplified. added xml output

--- parse_raw_data.pl
+++ parse_raw_data.pl
@@ -4,46 +4,21 @@ use strict;
 
 use Win32::OLE;
 use Text::CSV;
+use IO::File;
+use XML::Writer;
 use Getopt::Long;
 use File::Basename;
 use File::Spec;
-    use Cwd;
+use Cwd;
+use Encode qw(encode decode is_utf8);
 
-my ($Period, $USDExchangeRate, $EURExchangeRate, $RUBExchangeRate, $ExcelFileName);
+my $ExcelFileName;
 
-GetOptions (  "period=s"  => \$Period,
-			  "usdrate=f"  => \$USDExchangeRate,
-			  "eurrate=f"  => \$EURExchangeRate,
-			  "rubrate=f"  => \$RUBExchangeRate,
-			  "excelfilename=s"  => \$ExcelFileName
+GetOptions (  "excelfilename=s"  => \$ExcelFileName
 			  ) or &{ 
 					print_usage();
 					exit(1)
 					};
-
-if (!$Period) {
-	print "Period value should be specified!\n";
-	print_usage();
-	exit(1);
-}
-
-if (!$USDExchangeRate) {
-	print "USD exchange rate should be specified!\n";
-	print_usage();
-	exit(1);
-}
-
-if (!$EURExchangeRate) {
-	print "EUR exchange rate should be specified!\n";
-	print_usage();
-	exit(1);
-}
-
-if (!$RUBExchangeRate) {
-	print "RUB exchange rate should be specified!\n";
-	print_usage();
-	exit(1);
-}
 
 if (!$ExcelFileName) {
 	print "Excel file name should be specified!\n";
@@ -69,6 +44,10 @@ my $csv = Text::CSV->new ( { binary => 1, eol => $/ } ) or die "Cannot use CSV: 
 
 open my $fh, ">", "result.csv" or die "result.csv: $!";
 
+my $writer = XML::Writer->new( OUTPUT => 'self', DATA_MODE => 1 );
+$writer->xmlDecl("UTF-8");
+$writer->startTag("records");
+
 my $Counter = 0;
 
 for (my $i=1; $i <= $ExcelBookOle->Sheets->{Count}; $i++ ) {
@@ -83,34 +62,57 @@ for (my $i=1; $i <= $ExcelBookOle->Sheets->{Count}; $i++ ) {
 		
 	my $Client = $sheet->Cells(3,2)->{Value};
 	my $ResponsiblePerson = $sheet->Cells(6,2)->{Value};
-	my $Currency = $sheet->Cells(1,41)->{Value};
+	
+	my ($currency_temp, $Currency);
+	
+	my $currency_temp = $sheet->Cells(1,41)->{Value};
+	if ($currency_temp =~ /ÃÐÍ/) {
+		$Currency = "980";
+	} elsif ($currency_temp =~ /ÄÎËÀÐ ÑØÀ/) {
+		$Currency = "840";
+	} 
 		
 	my %SheetData;	
 		
-	ExtractDataFromSheet($sheet, \%SheetData, $Currency, 1);
+	ExtractDataFromSheet($sheet, \%SheetData, 1);
 	
-	$sheet->Cells(1,9)->{Value} = 0;
-	$sheet->Cells(1,12)->{Value} = 0;
-	$sheet->Cells(1,15)->{Value} = 0;
-	$sheet->Cells(1,18)->{Value} = 0;
-	$sheet->Cells(1,21)->{Value} = 0;
-	$sheet->Cells(1,24)->{Value} = 0;
+	for my $discount_offset (0 .. 9) {
+		$sheet->Cells( 1, 9 + $discount_offset*3 )->{Value} = 0;
+	}
 	
 	$sheet->Calculate();
 	
-	ExtractDataFromSheet($sheet, \%SheetData, $Currency, 0);
+	ExtractDataFromSheet($sheet, \%SheetData, 0);
 
 	for my $row_num (keys %SheetData) {	
-	
-		my $discount = sprintf("%.2f", (1-$SheetData{$row_num}->{Price0}/$SheetData{$row_num}->{Price}) * -100) + 0;
-	
-		$csv->print($fh, [$Counter++, $Period, $sheet->{Name}, $Client, $ResponsiblePerson, $Currency, $SheetData{$row_num}->{Category}, $SheetData{$row_num}->{Service}, $SheetData{$row_num}->{Qty}, $SheetData{$row_num}->{Unit}, $SheetData{$row_num}->{Price}, $SheetData{$row_num}->{PriceUAH}, $SheetData{$row_num}->{PriceUSD}, $SheetData{$row_num}->{Price0}, $SheetData{$row_num}->{Price0UAH}, $SheetData{$row_num}->{Price0USD}, $discount]);
+		
+		$csv->print($fh, [$Counter++, $sheet->{Name}, $Client, $ResponsiblePerson, $Currency, $SheetData{$row_num}->{Category}, $SheetData{$row_num}->{Service}, $SheetData{$row_num}->{Qty}, $SheetData{$row_num}->{Unit}, $SheetData{$row_num}->{Price}, $SheetData{$row_num}->{Price0}]);
+		
+		$writer->startTag("record");
+		$writer->dataElement( counter => $Counter );
+		$writer->dataElement( sheetname => decode('windows-1251', $sheet->{Name}) );
+		$writer->dataElement( client => decode('windows-1251', $Client) );
+		$writer->dataElement( responsibleperson => decode('windows-1251', $ResponsiblePerson) );
+		$writer->dataElement( currency => $Currency );
+		$writer->dataElement( category => decode('windows-1251', $SheetData{$row_num}->{Category}) );
+		$writer->dataElement( service => decode('windows-1251', $SheetData{$row_num}->{Service}) );
+		$writer->dataElement( qty => $SheetData{$row_num}->{Qty} );
+		$writer->dataElement( unit => decode('windows-1251', $SheetData{$row_num}->{Unit}) );
+		$writer->dataElement( price => $SheetData{$row_num}->{Price} );
+		$writer->dataElement( price0 => $SheetData{$row_num}->{Price0} );
+		$writer->endTag("record");		
 		
 	}	
 		
 }
 
- close $fh or die "result.csv: $!";
+close $fh or die "result.csv: $!";
+ 
+$writer->endTag("records");
+
+open my $fhxml, ">:encoding(UTF-8)", "result.xml" or die "result.xml: $!";
+print $fhxml $writer->to_string;
+close $fhxml or die "result.xml: $!";
  
 $ExcelBookOle->Close(0);
 
@@ -122,12 +124,12 @@ $ExcelOle = undef;
 ######################################################
 
 sub print_usage {
-	print "Usage: perl $0 --excelfilename=excel_file_name --period=period_name --usdrate=usd_exchange_rate --eurrate=eur_exchange_rate --rubrate=rub_echange_rate\n";
+	print "Usage: perl $0 --excelfilename=excel_file_name\n";
 }
 
 sub ExtractDataFromSheet {
 
-	my ($sheet, $DataHashRef, $Currency, $mode) = @_;
+	my ($sheet, $DataHashRef, $mode) = @_;
 
 	my $Category = '';
 	my $Item;	
@@ -161,31 +163,14 @@ sub ExtractDataFromSheet {
 					$Qty = 1;
 				}
 				$Unit = $sheet->Cells($row,4)->{Value};
-				if ($Currency =~ /ÃÐÍ/) {
-					$PriceUAH = $Price;
-					$PriceUSD = sprintf("%.2f", $PriceUAH/$USDExchangeRate)+0;
-				} elsif ($Currency =~ /ÄÎËÀÐ ÑØÀ/) {
-					$PriceUAH = sprintf("%.2f", $Price*$USDExchangeRate)+0;
-					$PriceUSD = $Price;				
-				} elsif ($Currency =~ /EUR/) {
-					$PriceUAH = sprintf("%.2f", $Price*$EURExchangeRate)+0;
-					$PriceUSD = sprintf("%.2f", $Price*$EURExchangeRate/$USDExchangeRate)+0;				
-				} elsif ($Currency =~ /RUB/) {
-					$PriceUAH = sprintf("%.2f", $Price*$RUBExchangeRate)+0;
-					$PriceUSD = sprintf("%.2f", $Price*$RUBExchangeRate/$USDExchangeRate)+0;				
-				}
 				$DataHashRef->{$row}->{Category} = $Category;
 				$DataHashRef->{$row}->{Service} = $Item;
 				$DataHashRef->{$row}->{Qty} = $Qty;
 				$DataHashRef->{$row}->{Unit} = $Unit;
 				if ($mode) {
 					$DataHashRef->{$row}->{Price} = $Price;
-					$DataHashRef->{$row}->{PriceUAH} = $PriceUAH;
-					$DataHashRef->{$row}->{PriceUSD} = $PriceUSD;
 				} else {
 					$DataHashRef->{$row}->{Price0} = $Price;
-					$DataHashRef->{$row}->{Price0UAH} = $PriceUAH;
-					$DataHashRef->{$row}->{Price0USD} = $PriceUSD;				
 				}	
 			}
 		}
